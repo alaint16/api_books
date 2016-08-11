e0f1950c97b87a8f444d362d81080286b3729110
alaint16 <atovar@optimeconsulting.com>
2016-08-11 16:25:16 -0400

    finished app
    finished API book with title and link and css

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
     $(".srbook").click(function() {
         var book = $(".bookInput").val();
-        console.log(book)
+        console.log(book);
         
-         $.get('http://www.openlibrary.org/b/OL7668717M', function(dataresults){
-         	$(".result_cover").append("<div>" + book + " " + dataresults.bookName + "</div>");
+         $.get('https://www.googleapis.com/books/v1/volumes?q=' + book, function(dataresults){
+                console.log(dataresults);
+         for(var i = 0; i < dataresults.items.length; i++){
+
+            $(".results").append('<li>'+
+'<div class="result_cover"><img src="'+ dataresults.items[i].volumeInfo.imageLinks.thumbnail +'"></div>'+
+     '<div class="description">' +
+    '<div class="result_title">'+ "<a target='_blank' href=' "+ dataresults.items[i].volumeInfo.canonicalVolumeLink + " '> " + dataresults.items[i].volumeInfo.title + "</a>" + '</div>'+
+    '<div class="result_author">'+ dataresults.items[i].volumeInfo.authors +'</div>' + '</div>' + '</li>'
+
+
+            );
+
+
+         }
+  dataresults++;
+
+    
          	$(".bookInput").val(" ");
          
     //  showResults(time.Search);
